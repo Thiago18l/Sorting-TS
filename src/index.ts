acbbe4cfc38a195c5f3108db858936f37610963f
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-09-01 14:16:37 -0300

    feat: add constructor and add a method to sort an array

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
 class Sorter {
+    private collection: number[];
 
+    constructor(collection: number[]) {
+        this.collection = collection;
+    }
+    sort(): void {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const temp = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = temp;
+                }
+            }
+        }
+    }
+    public getCollection () {
+        return this.collection;
+    }
 }
+
+const sorting = new Sorter([1, 4, 3, 6]);
+sorting.sort();
+console.log(sorting.getCollection());
