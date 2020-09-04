d1fde5caf519f71e188ac83928fe76aa03765a77
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-09-03 22:12:42 -0300

    fix: fix the call of method

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ import { NumbersCollection } from './NumbersCollection';
 const array = new NumbersCollection([1, 2, 4, 5, 8, 2, 3]);
 const sorting = new Sorter(array);
 sorting.sort();
-console.log(sorting.array);
+console.log(sorting);
