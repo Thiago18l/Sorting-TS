539819c0bb803dea63ee144509dfd5103ae5f40c
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-09-04 23:50:09 -0300

    feat: remove interface and implement Sorter like a abstract class

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,21 +1,14 @@
-interface ISortable {
-    length: number;
-    compare(left: number, right: number): boolean;
-    swap(left: number, right: number): void;
-}
+export abstract class Sorter {
+    abstract compare(left: number, right: number): boolean;
+    abstract swap(left: number, right: number): void;
+    abstract length: number;
 
-export class Sorter {
-    private collection: ISortable;
-
-    constructor(collection: ISortable) {
-        this.collection = collection;
-    }
     sort(): void {
-        const { length } = this.collection;
+        const { length } = this;
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection.compare(j, j + 1)) {
-                   this.collection.swap(j, j + 1);
+                if (this.compare(j, j + 1)) {
+                   this.swap(j, j + 1);
                 }
             }
         }
